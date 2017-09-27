2d6904013515b57ce9a52fef3893a08c9fe91591
DK <kamkov.dmytro@gmail.com>
2017-09-27 17:56:12 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+	 <link href="css/bootstrap.css" rel="stylesheet">
+	 <link href="css/font-awesome.min.css" rel="stylesheet">
+	 <link href="css/main.css" rel="stylesheet">
+	 <link href="slick/slick.css" rel="stylesheet"  />
+	 <link href="slick/slick-theme.css" rel="stylesheet"/>
+
+</head>
+<body>
+	<div class="navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				</button>
+				<!-- LOGO-->
+				<a class="navbar-brand" href="#"></a>
+			</div>
+			<div class="navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active" =><a href="#">Главная</a></li>
+					<li><a href="#news">Новости</a></li>
+					<li><a href="#contacts"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+	<div id="headerwrap">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-8 col-lg-offset-2">
+					<h1>Lorem ipsum dolor</h1>
+					<h2>Excepteur sint occaecat cupidatat</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div id="news">
+		<div class="continer padding-t-35">
+			<div class="row centered">
+				<br><br>
+				<div class="col-lg-4">
+					<i class="fa fa-heart">
+						<h4>Bonorum et Malorum</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</i>
+				</div>
+				<div class="col-lg-4">
+					<i class="fa fa-laptop">
+						<h4>Bonorum et Malorum</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</i>
+				</div>
+				<div class="col-lg-4">
+					<i class="fa fa-trophy">
+						<h4>Bonorum et Malorum</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</i>
+				</div>
+			</div>
+			<br><br>
+		</div>
+	</div>
+
+	<div id="contacts">
+		<div class="container padding-t-35">
+			<div class="row centered">
+				<br><br>
+				<div class="col-lg-8 col-lg-offset-2"></div>
+				<h2><span>Контакты</span></h2>
+				<form method="post">
+					<input class="form-control width-6 margin-b-20" type="text" id="name" placeholder="Имя"  required>
+					<input class="form-control width-6 margin-b-20" type="email" id="email" placeholder="Почта" required>
+					<textarea class="form-control width-6  margin-b-20" type="text" id="message" placeholder="Ваше сообщение..."  rows="5" required></textarea>	
+					<button type="submit" value="Отправить"></button>				
+				</form>
+				<div class="col-lg-2"></div>
+				<div class="col-lg-10 col-lg-offset-1">
+					<img src="" alt="" class="img-responsive">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="footer">
+		<div class="container">
+			<div class="row centered">
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-vk"></i></a>
+			</div>
+		</div>
+	</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/js.js"></script>
+</body>
+</html>	
---
+++ js/js.js
@@ -0,0 +1,8 @@
+/*-----------Forms-----------*/
+$('#message, #email').focus()
+ $('input').focus(function() { $(this).css('background', '#fff') } )
+ $('input') .blur(function() { $(this).css('background', '#B4FEB5') } )
+
+ $('#message').focus()
+ $('textarea').focus(function() { $(this).css('background', '#fff') } )
+ $('textarea') .blur(function() { $(this).css('background', '#B4FEB5') } )
